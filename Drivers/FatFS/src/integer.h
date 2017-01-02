15f6c6bf9c27d8f17532565f559e5bfd4e24d7be
Erich Styger <erich.styger@hslu.ch>
2017-01-02 09:01:41 +0100

    special handling of FatFS types for IAR

--- Drivers/FatFS/src/integer.h
+++ Drivers/FatFS/src/integer.h
@@ -19,20 +19,40 @@ typedef int				INT;
 typedef unsigned int	UINT;
 
 /* This type MUST be 8-bit */
-typedef unsigned char	BYTE;
+#ifdef __ICCARM__
+  typedef __UINT8_T_TYPE__	BYTE;
+#else
+  typedef unsigned char	BYTE;
+#endif
 
 /* These types MUST be 16-bit */
-typedef short			SHORT;
-typedef unsigned short	WORD;
-typedef unsigned short	WCHAR;
+#ifdef __ICCARM__
+  typedef __INT16_T_TYPE__	SHORT;
+  typedef __UINT16_T_TYPE__	WORD;
+  typedef __UINT16_T_TYPE__	WCHAR;
+#else
+  typedef short			SHORT;
+  typedef unsigned short	WORD;
+  typedef unsigned short	WCHAR;
+#endif
 
 /* These types MUST be 32-bit */
-typedef long			LONG;
-typedef unsigned long	DWORD;
+#ifdef __ICCARM__
+  typedef __INT32_T_TYPE__	LONG;
+  typedef __UINT32_T_TYPE__	DWORD;
+#else
+  typedef long			LONG;
+  typedef unsigned long	DWORD;
+#endif
 
 /* This type MUST be 64-bit (Remove this for C89 compatibility) */
-typedef unsigned long long QWORD;
-
+#ifdef __ICCARM__
+  typedef __UINT64_T_TYPE__ QWORD;
+#else
+  typedef unsigned long long QWORD;
 #endif
 
-#endif
+#endif /* _WIN32 */
+
+#endif /* _FF_INTEGER */
+
