daee8e9abdbee52f0b0c19db2b9629e83dc5230f
Erich Styger <erich.styger@hslu.ch>
2017-08-16 18:50:02 +0200

    added C++ wrappers

--- Drivers/freeRTOS/Source/portable/GCC/ARM_CM4F/portTicks.h
+++ Drivers/freeRTOS/Source/portable/GCC/ARM_CM4F/portTicks.h
@@ -70,6 +70,10 @@
 #ifndef PORTTICKS_H_
 #define PORTTICKS_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /*
  *  Interface header file to the Processor Expert Tick counter.
  *  This file is used to access the interface, especially for performance
@@ -189,5 +193,9 @@ portLONG uxGetTickCounterValue(void);
   #define TRC_HWTC_FREQ_HZ            FREERTOS_HWTC_FREQ_HZ
 #endif
 #endif /* configUSE_TRACE_HOOKS */
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* PORTTICKS_H_ */
